8e8211a8a2e0dd04603148078007366366e26f5a
danPixl <d@danpixl.me>
2017-06-22 16:03:36 -0600

    config stuff

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 console.log('Getting updates..')
 
+// git pull
 require('simple-git')(__dirname)
      .pull()
+
+// update modules
 var npm = require('npm')
 npm.load(function(err) {
     npm.commands.update([], function(er, data) {
@@ -12,10 +15,29 @@ npm.load(function(err) {
     })
 })
 
+// delete lock file created by npm
+console.log('Cleaning up...')
+try {
 var fs = require('fs'),
-    filename = "./package-lock.json",
-    tempFile = fs.openSync(filename, 'r')
+    lockFile = "./package-lock.json",
+    stopFunkFile = fs.openSync(lockFile, 'r')
 
-fs.closeSync(tempFile);
+fs.closeSync(stopFunkFile)
+fs.unlinkSync(lockFile)
+} catch (e) {
+    console.log('Nothing to clean up.')
+}
+console.log('Defining global variables...')
+global.Discord = require("discord.js")
+global.client = new Discord.Client({disableEveryone: true})
 
-fs.unlinkSync(filename);
+console.log('Initializing config...')
+try {
+    global.config = JSON.parse(fs.readFileSync('./config.json'))
+} catch (e) {
+    console.log("We couldn't find your configuration. Please make sure that you've copied the config.json.example to config.json")
+    console.log("Here's the exact error:")
+    console.log(e)
+    console.log("Stopping...")
+    process.exit()
+}
