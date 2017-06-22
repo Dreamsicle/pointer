c528af93638fb183e0aed98a87faa845c7012424
danPixl <d@danpixl.me>
2017-06-22 16:45:24 -0600

    base bot

--- bot.js
+++ bot.js
@@ -1,4 +1,20 @@
 console.log("Success!")
 client.on('ready', () => {
-  console.log('Ready.')
+    console.log('Ready.')
+})
+
+client.on('message', message => {
+    // handle prefix
+    if (message.content.substring(0, prefix.length) !== prefix) { /* if the first two characters aren't 'g/', just ignore it */
+        return
+    } else { /* if they are, */
+        message.content = message.content.substr(prefix.length) /* remove them (to keep the clutter of prefix + 'actual command' away) and carry on */
+    }
+
+    if (message.content === 'ping') {
+        message.channel.send("", {embed: {
+            title: 'Pong!',
+            color: 16706817
+        }})
+    }
 })
