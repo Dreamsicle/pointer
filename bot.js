cc03edc5e59a6a27503f2a451d3187566bd5f2a0
danPixl <d@danpixl.me>
2017-06-23 23:58:34 -0600

    beanhammer added to aresenal

--- bot.js
+++ bot.js
@@ -20,13 +20,27 @@ client.on('message', message => {
 
     if (message.content.substring(0,3) === 'ban') {
         message.content = message.content.substr(4) // remove 'ban' and the trailing space to just get the member to be beaned
-        if (message.author.hasPermission("BAN_MEMBERS")) {
-            console.log("ayy")
+        if (message.member.hasPermission("BAN_MEMBERS")) {
+            try {
+                message.guild.member(message.mentions.users.first()).kick()
+                message.channel.send("", {embed: {
+                    title: 'Press F to pay respects.',
+                    color: 0x930e67
+                }})
+            } catch (e) {
+                message.channel.send("", {embed: {
+                    title: 'Something went wrong.',
+                    description: 'Please check if I have the proper permissions! If that seems to be fine, here\'s the full error: \n' + e,
+                    color: 0xc5283d
+                }})
+            }
+        } else {
+            message.channel.send("", {embed: {
+                title: "Sorry, but you don't have permission to do this.",
+                description: "Please check if you have permsission to ban members.",
+                color: 0xc5283d
+            }})
         }
-        message.channel.send("", {embed: {
-            title: 'Press F to pay respects.',
-            color: 0x930e67
-        }})
     }
 
 })
